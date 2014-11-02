a3c01824c8f79e863568785e78d93848d757e90e
jejeLaVedette <barbier_jerome@hotmail.fr>
2014-11-02 17:52:55 +0100

    ajout des clients dans la liste côté server

--- nioImplementation/src/multiCast/server/Server.java
+++ nioImplementation/src/multiCast/server/Server.java
@@ -3,23 +3,30 @@ package multiCast.server;
 import multiCast.Entities;
 import multiCast.nioImplementation.NioEngineImp;
 import multiCast.server.kernel.callbackServer.ServerAcceptCallbackImp;
+import nio.engine.NioChannel;
 import nio.engine.NioEngine;
 import nio.engine.NioServer;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by augustin on 30/10/14.
  */
 public class Server extends Entities{
+	
+	private ArrayList<NioChannel> clientList;
+	private int maxClientRoom;
 
     public Server(int identity){
         this.identity = identity;
         this.clock = 0;
         this.addressHashMap = new HashMap<String, InetAddress>();
         this.integerHashMap = new HashMap<String, Integer>();
-    }
+        clientList = new ArrayList<NioChannel>();
+        maxClientRoom = 3;
+        }
 
    
     public void run(){
@@ -31,7 +38,7 @@ public class Server extends Entities{
             NioEngine.panic("Error during the creation of the server");
         }
 
-        NioServer server = nioEngine.listen(new ServerAcceptCallbackImp());
+        NioServer server = nioEngine.listen(new ServerAcceptCallbackImp(this));
         if(server == null){
             NioEngine.panic("creation server failed");
             System.exit(-1);
@@ -40,4 +47,10 @@ public class Server extends Entities{
         System.out.println("server launch");
         nioEngine.mainloop();
     }
+
+
+	public ArrayList<NioChannel> getClientList() {
+		// TODO Auto-generated method stub
+		return this.clientList;
+	}
 }
